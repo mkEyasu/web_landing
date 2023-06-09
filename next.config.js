1df90799b7f65957059455e54a608a4ac1dd0eea
eyasu-c <gbtech58@gmail.com>
2023-06-09 14:06:04 +0300

    web_landing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  basePath: "/web_landing",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
