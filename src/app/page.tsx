9097c10c105ee8fba86c85f425fd45441972f6f3
eyasu-c <gbtech58@gmail.com>
2023-06-09 14:33:40 +0300

    web_landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/images/vercel.svg"
+              src="https://mkeyasu.github.io/web_landing/images/vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +31,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/images/next.svg"
+          src="https://mkeyasu.github.io/web_landing/images/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
